f4f10c360a4b282d697991a89f9cfc98e408a62a
skmar10 <skmar10@student.monash.edu>
2016-04-06 22:28:35 +0800

    Added files via upload

---
+++ Pages/CookieTheftPage.xaml.cs
@@ -0,0 +1,310 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Core;
+using Windows.Media.SpeechRecognition;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Windows.UI.Xaml.Media;
+using Windows.Globalization;
+using Windows.UI.Xaml.Documents;
+using System.Threading.Tasks;
+using Windows.UI;
+using Windows.UI.Xaml.Controls.Primitives;
+
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace DIAGmentia_UI_2.Pages
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class CookieTheftPage : Page
+    {
+        int Clicked;
+        // Speech events may come in on a thread other than the UI thread, keep track of the UI thread's
+        // dispatcher, so we can update the UI in a thread-safe manner.
+        private CoreDispatcher dispatcher;
+
+        // The speech recognizer used throughout this sample.
+        private SpeechRecognizer speechRecognizer;
+
+        // Keep track of whether the continuous recognizer is currently running, so it can be cleaned up appropriately.
+        private bool isListening;
+
+        // Keep track of existing text that we've accepted in ContinuousRecognitionSession_ResultGenerated(), so
+        // that we can combine it and Hypothesized results to show in-progress dictation mid-sentence.
+        private StringBuilder dictatedTextBuilder;
+
+        public CookieTheftPage()
+        {
+            this.InitializeComponent();
+            Clicked = 0;
+
+            
+        }
+
+        private void ProgressBar_ValueChanged(object sender, RangeBaseValueChangedEventArgs e)
+        {
+
+        }
+
+        private async void Test_Control_Click(object sender, RoutedEventArgs e)
+        {
+            if (Clicked == 0)
+            {
+                Clicked = 1;
+                TestControlBtn.Background = ColorToBrush("#e74c3c");
+                TestControlBtn.Content = "STOP";              
+                await InitializeRecognizer();
+                ContinuousRecognize_Clicked();
+            }
+            else if (Clicked == 1) {
+                Clicked = 0;
+                TestControlBtn.Background = ColorToBrush("#3498db");
+                TestControlBtn.Content = "START";
+                ContinuousRecognize_Clicked();
+
+            }
+
+        }
+
+
+        public static Brush ColorToBrush(string color) // color = "#E7E44D"
+        {
+            color = color.Replace("#", "");
+            if (color.Length == 6)
+            {
+                return new SolidColorBrush(ColorHelper.FromArgb(255,
+                    byte.Parse(color.Substring(0, 2), System.Globalization.NumberStyles.HexNumber),
+                    byte.Parse(color.Substring(2, 2), System.Globalization.NumberStyles.HexNumber),
+                    byte.Parse(color.Substring(4, 2), System.Globalization.NumberStyles.HexNumber)));
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Initialize Speech Recognizer and compile constraints.
+        /// </summary>
+        /// <param name="recognizerLanguage">Language to use for the speech recognizer</param>
+        /// <returns>Awaitable task.</returns>
+        private async Task InitializeRecognizer()
+        {
+            if (speechRecognizer != null)
+            {
+                this.speechRecognizer.Dispose();
+                this.speechRecognizer = null;
+            }
+
+            this.speechRecognizer = new SpeechRecognizer();
+
+            
+
+            // Apply the dictation topic constraint to optimize for dictated freeform speech.
+            var dictationConstraint = new SpeechRecognitionTopicConstraint(SpeechRecognitionScenario.Dictation, "dictation");
+            speechRecognizer.Constraints.Add(dictationConstraint);
+            SpeechRecognitionCompilationResult result = await speechRecognizer.CompileConstraintsAsync();
+            if (result.Status != SpeechRecognitionResultStatus.Success)
+            {
+                dictationTextBox.Text = "Dictation Failed";
+            }
+
+            // Handle continuous recognition events. Completed fires when various error states occur. ResultGenerated fires when
+            // some recognized phrases occur, or the garbage rule is hit. HypothesisGenerated fires during recognition, and
+            // allows us to provide incremental feedback based on what the user's currently saying.
+            speechRecognizer.ContinuousRecognitionSession.Completed += ContinuousRecognitionSession_Completed;
+            speechRecognizer.ContinuousRecognitionSession.ResultGenerated += ContinuousRecognitionSession_ResultGenerated;
+            speechRecognizer.HypothesisGenerated += SpeechRecognizer_HypothesisGenerated;
+        }
+
+
+        /// <summary>
+        /// Upon leaving, clean up the speech recognizer. Ensure we aren't still listening, and disable the event 
+        /// handlers to prevent leaks.
+        /// </summary>
+        /// <param name="e">Unused navigation parameters.</param>
+        protected async override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            if (this.speechRecognizer != null)
+            {
+                if (isListening)
+                {
+                    await this.speechRecognizer.ContinuousRecognitionSession.CancelAsync();
+                    isListening = false;
+
+                }
+
+                dictationTextBox.Text = "";
+
+                speechRecognizer.ContinuousRecognitionSession.Completed -= ContinuousRecognitionSession_Completed;
+                speechRecognizer.ContinuousRecognitionSession.ResultGenerated -= ContinuousRecognitionSession_ResultGenerated;
+                speechRecognizer.HypothesisGenerated -= SpeechRecognizer_HypothesisGenerated;
+                
+
+                this.speechRecognizer.Dispose();
+                this.speechRecognizer = null;
+            }
+         }
+             /// <summary>
+             /// Handle events fired when error conditions occur, such as the microphone becoming unavailable, or if
+             /// some transient issues occur.
+             /// </summary>
+             /// <param name="sender">The continuous recognition session</param>
+             /// <param name="args">The state of the recognizer</param>
+        private async void ContinuousRecognitionSession_Completed(SpeechContinuousRecognitionSession sender, SpeechContinuousRecognitionCompletedEventArgs args)
+        {
+            if (args.Status != SpeechRecognitionResultStatus.Success)
+            {
+                // If TimeoutExceeded occurs, the user has been silent for too long. We can use this to 
+                // cancel recognition if the user in dictation mode and walks away from their device, etc.
+                // In a global-command type scenario, this timeout won't apply automatically.
+                // With dictation (no grammar in place) modes, the default timeout is 20 seconds.
+                if (args.Status == SpeechRecognitionResultStatus.TimeoutExceeded)
+                {
+                    await dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+                    {
+                        dictationTextBox.Text = dictatedTextBuilder.ToString();
+                        isListening = false;
+                    });
+                }
+                else
+                {
+                    await dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+                    {      
+                        isListening = false;
+                    });
+                }
+            }
+        }
+
+
+        /// <summary>
+        /// While the user is speaking, update the textbox with the partial sentence of what's being said for user feedback.
+        /// </summary>
+        /// <param name="sender">The recognizer that has generated the hypothesis</param>
+        /// <param name="args">The hypothesis formed</param>
+        private async void SpeechRecognizer_HypothesisGenerated(SpeechRecognizer sender, SpeechRecognitionHypothesisGeneratedEventArgs args)
+        {
+            string hypothesis = args.Hypothesis.Text;
+
+            // Update the textbox with the currently confirmed text, and the hypothesis combined.
+            string textboxContent = dictatedTextBuilder.ToString() + " " + hypothesis + " ...";
+            await dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+            {
+                dictationTextBox.Text = textboxContent;
+            });
+        }
+
+
+        /// <summary>
+        /// Handle events fired when a result is generated. Check for high to medium confidence, and then append the
+        /// string to the end of the stringbuffer, and replace the content of the textbox with the string buffer, to
+        /// remove any hypothesis text that may be present.
+        /// </summary>
+        /// <param name="sender">The Recognition session that generated this result</param>
+        /// <param name="args">Details about the recognized speech</param>
+        private async void ContinuousRecognitionSession_ResultGenerated(SpeechContinuousRecognitionSession sender, SpeechContinuousRecognitionResultGeneratedEventArgs args)
+        {
+            // We may choose to discard content that has low confidence, as that could indicate that we're picking up
+            // noise via the microphone, or someone could be talking out of earshot.
+            if (args.Result.Confidence == SpeechRecognitionConfidence.Medium ||
+                args.Result.Confidence == SpeechRecognitionConfidence.High)
+            {
+                dictatedTextBuilder.Append(args.Result.Text + " ");
+
+                await dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+                {
+
+                    dictationTextBox.Text = dictatedTextBuilder.ToString();
+
+                });
+            }
+            else
+            {
+                // In some scenarios, a developer may choose to ignore giving the user feedback in this case, if speech
+                // is not the primary input mechanism for the application.
+                // Here, just remove any hypothesis text by resetting it to the last known good.
+                await dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+                {
+                    dictationTextBox.Text = dictatedTextBuilder.ToString();
+                    string discardedText = args.Result.Text;
+                    if (!string.IsNullOrEmpty(discardedText))
+                    {
+                        discardedText = discardedText.Length <= 25 ? discardedText : (discardedText.Substring(0, 25) + "...");
+                        dictationTextBox.Text = "Discarded due to low/rejected Confidence: " + discardedText;
+
+                    }
+                });
+            }
+        }
+
+
+
+        public async void ContinuousRecognize_Clicked()
+            {
+            
+            if (isListening == false)
+            {
+                // The recognizer can only start listening in a continuous fashion if the recognizer is currently idle.
+                // This prevents an exception from occurring.
+                if (speechRecognizer.State == SpeechRecognizerState.Idle)
+                {
+
+                    isListening = true;
+                    await speechRecognizer.ContinuousRecognitionSession.StartAsync();
+                    
+                    
+                }
+            }
+            else
+            {
+                isListening = false;
+                
+                if (speechRecognizer.State != SpeechRecognizerState.Idle)
+                {
+                   
+                        await speechRecognizer.ContinuousRecognitionSession.StopAsync();
+                        dictationTextBox.Text = dictatedTextBuilder.ToString();
+                    }
+                    
+                }
+            }
+            
+        }
+
+    }
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
